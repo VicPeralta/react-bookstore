ba38d7a245b3a5796fe8285edc410cd02dee8ccb
Victor Peralta <victor.peralta.gomez@gmail.com>
2022-03-28 18:05:36 -0600

    Change createStore to configureStore
    modified:   src/redux/configureStore.js

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,8 +1,9 @@
-import { createStore, combineReducers } from 'redux';
+import { combineReducers } from 'redux';
+import { configureStore } from '@reduxjs/toolkit';
 import * as Books from './books/books';
 import * as Categories from './categories/categories';
 
-const rootReducer = combineReducers(Books.default, Categories.default);
-const store = createStore(rootReducer);
+const rootReducer = combineReducers({ books: Books.default, categories: Categories.default });
+const store = configureStore({ reducer: rootReducer });
 
 export default store;
